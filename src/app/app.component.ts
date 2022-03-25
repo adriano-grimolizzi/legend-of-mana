64e085fb95c2551fa854f54a7392cd8afcac3c15
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2022-03-25 11:38:40 +0100

    add list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,15 @@ import { TemplateItem } from './model/TemplateItem';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   testTemplateItem: TemplateItem = {
     key: 'Condition',
-    value: '40 C-30 HR+'
-  }
+    value: '40 C-30 HR+',
+  };
+
+  items: TemplateItem[] = [
+    { key: 'Application', value: 'None' },
+    { key: 'AnalysisDepartment', value: 'LDT' },
+  ];
 
   title = 'legend-of-mane';
 }
